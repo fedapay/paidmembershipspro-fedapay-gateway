fbb88245a402eb09fa7788dcabde2ddda7f6e927
Boris Koumondji <brexis@ip-172-31-28-104.us-east-2.compute.internal>
2020-11-22 04:39:43 +0000

    Remove WC requirement

--- pmpro-fedapay-gateway.php
+++ pmpro-fedapay-gateway.php
@@ -7,8 +7,6 @@
  * Author URI: https://fedapay.com/
  * Requires at least: 4.4
  * Tested up to: 5.4.2
- * WC requires at least: 2.6
- * WC tested up to: 4.3.0
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: pmpro-fedapay-gateway
